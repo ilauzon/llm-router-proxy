2b32e9231bba72f07c20af67c2ef6e28023bcc49
ilauzon <ilauzonvc@gmail.com>
2025-11-23 22:05:03 -0800

    Fix formatting of user-facing strings

--- src/lang/fmt.ts
+++ src/lang/fmt.ts
@@ -1,7 +1,7 @@
 export const fmt = (str: string, ...params: any[]) => {
     for (let i = 0; i < params.length; i++) {
         const param = params[i]
-        str.replaceAll(`\$${i}`, param)
+        str = str.replaceAll(`%${i + 1}`, param)
     }
     return str
 }
